3126abcaa8f833c9dd5c14f5a67f32c2fde27fa9
Alice Julien-Laferriere <alice.julien-laferriere@roche.com>
2020-04-03 16:02:47 +0200

    Adding threshold exporting for immune annotation; function will write predefinied threhsold in a csv file whichwill be read afterward for cell popluation assignation

--- besca/tl/sig/__init__.py
+++ besca/tl/sig/__init__.py
@@ -1,6 +1,7 @@
 from ._sig import combined_signature_score, compute_signed_score
 from ._io_sig import read_GMT_sign
 from ._annot import getset, score_mw, add_anno, make_anno
+from ._sig_thresholds import generate_annot_threshold
 
 __all__ = ["combined_signature_score",
            "compute_signed_score",
@@ -8,5 +9,8 @@ __all__ = ["combined_signature_score",
            'getset',
            'score_mw',
            'add_anno',
-           'make_anno'
+           'make_anno',
+           'generate_annot_threshold'
            ]
+
+           
---
+++ besca/tl/sig/_sig_thresholds.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+def generate_annot_threshold(myc, fileName='sig_threshold.csv'):
+    """ Generate a config files for autoannotation of the clusters.
+    This file will be used afterwards by the sig modules.
+    Modifying the thresholds is then possible on the text file
+    to take into account datasets specificity.
+    parameters
+    ----------
+    myc: 'numpy.float64'
+      threshold used for cluster attribution
+    fileName: 'str'
+      where this will be saved.
+    returns
+    -------
+    list of str
+        String of cluster labels
+    """
+    cellName = pd.Series(['Bcells', 'Plasma', 'Tcd8', 'Tcd4',
+                'Tcgd', 'Treg', 'TNK', 'TilCM', 'T4CM',
+                'Tcytox', 'TEM', 'Ttexh', 'Tpexh', 
+                'NKT', 'Tcells', 'NKcells', 'NKnai',
+                'NKcyt', 'Myelo', 'Naive', 'Activation',
+                'Memory', 'Eff', 'nonEff', 'Cytotox',
+                'aDCs', 'moDC', 'pDCs', 'cDC1', 
+                'cDC2', 'Monocytes', 'TAM', 'TMO',
+                'Mo14', 'Mo16', 'TAMCx', 'TMid',
+                'MyeloSubtype', 'Granulo',  'Neutrophil', 'Macrophage',
+                'Cellcycle'  , 'Checkpoint', 'Ifng',
+                'Endo', 'Cafs',  'MelMelan', 'Megakaryocytes'
+                ])
+    threshold = pd.Series([1, 1, 1, 1/2, 2/3,
+                 1, 2, 2, 3/2,
+                 2, 2, 1, 1, 3/2, 4/3, 2, 1,#Nknai
+                 2, 1, 1, 1, 1, 1, 1, 2,#Cytotox
+                 1, 3/2, 2, 1, 2, 1, 3/2, 1,#TMO
+                 1, 1, 1, 2,# Moma
+                 1, 1, 1, 1,# Macrophage
+                 4/3, 1, 1,#Ifng
+                 1, 1, 1, 1# Megakaryocytes
+                ])
+    threshold = pd.Series([x * myc for x in threshold])
+    finalTh = pd.concat([cellName, threshold] , axis = 1)
+    finalTh.to_csv(fileName)
+
